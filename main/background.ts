2dd4e3dbf57efb747931cc6adda1598871aa7a69
Jan Bujnovsky <jan.bujnovsky@hotovo.com>
2024-02-21 19:46:11 +0100

    SCAN-36: merged performScan and performDetectScanners to perform-manage-scanning util and fixed scanning bug

--- main/background.ts
+++ main/background.ts
@@ -1,7 +1,7 @@
 import { app } from 'electron';
 import serve from 'electron-serve';
 import { createWindow } from './helpers';
-import { performScan } from './utils/perform-scan';
+import { performScan, performDetectScanners } from './utils/perform-manage-scanning';
 import { ipcMain } from 'electron';
 import { addFileToQueue, performCancelRecognizeBraille } from './utils/perform-braille-recognition';
 import { performCancelInitialSetup, performInitialSetup } from './utils/perform-initial-setup';
@@ -12,7 +12,6 @@ import { performReadDocuments, performUpdateDocument } from './utils/perform-man
 import { performExportDocument } from './utils/perform-export-document';
 import { performLogFromRenderer } from './utils/perform-log-from-renderer';
 import { performCopyUploadedImage } from './utils/perform-copy-uploaded-image';
-import { scannerApp, performDetectScanners } from './utils/perform-detect-scanners';
 import { store } from './utils/store';
 import { performDeleteDocument } from './utils/perform-delete-document';
 import { logger } from './logger';
@@ -66,7 +65,7 @@ if (IS_PROD) {
     await performInitialSetup(mainWindow);
   });
   ipcMain.handle('scan-file', async (event, documentID, pageID, selectedScanner) => {
-    performScan(documentID, pageID, scannerApp, selectedScanner);
+    return performScan(documentID, pageID, selectedScanner);
   });
   ipcMain.on('recognize-braille', async (event, fileName, documentID, pageID) => {
     addFileToQueue(fileName, documentID, pageID, mainWindow);
