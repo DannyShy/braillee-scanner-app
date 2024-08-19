2c73cd597b3738f46b1e78ac488e15b2bebba72c
pavol.kosc <pavol.kosc@hotovo.com>
2024-08-19 10:47:20 +0000

    feature/SCAN_42_fixed_pdfToImage

--- main/utils/perform-prepare-file-for-recognition.ts
+++ main/utils/perform-prepare-file-for-recognition.ts
@@ -1,12 +1,11 @@
-// import pdf from 'pdf-poppler' - todo: not supported on linux;
 import fs from 'fs';
 import { logger } from '../logger';
 import path from 'path';
-import pdfImage from 'pdf-image';
 import { addFileToQueue } from './perform-braille-recognition';
 import { BrowserWindow } from 'electron';
 import { MY_DOCUMENTS_PATH } from './constants';
 import { performUpdateDocument } from './perform-manage-document';
+import { fromPath } from 'pdf2pic';
 
 let numberOfFiles;
 const supportedExtensions = ['.pdf', '.jpeg', '.jpg', '.png', '.gif', '.bmp', '.tiff', '.ico', '.jfif', '.webp'];
@@ -75,12 +74,17 @@ const performPrepareFileForRecognition = async (
 const performConvertPdfToImages = async (filePath: string, documentID: number) => {
   try {
     const outputDir = path.join(MY_DOCUMENTS_PATH, documentID.toString(), 'images');
-    // todo: find better alternative or implement selective method
-    // await pdf.convert(filePath, {
-    //   format: 'png',
-    //   out_dir: outputDir,
-    //   out_prefix: 'scan',
-    // });
+    const conversionOptions = {
+      density: 100,
+      saveFileName: 'scan',
+      savePath: outputDir,
+      format: "png",
+    }
+    const convert = fromPath(outputDir, conversionOptions);
+    await convert(1, { responseType: 'image' }).then((resolve) => {
+      return resolve;
+    })
+    
     const files = fs.readdirSync(outputDir);
 
     const pngFiles = files.filter((file) => path.extname(file).toLowerCase() === '.png');
